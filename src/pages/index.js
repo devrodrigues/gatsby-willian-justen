0202b7ba646772746f3d8cce1cb5b5403f899c66
Vinicius <vrf92@msn.com>
2020-07-29 22:07:56 -0300

    Uso inicial de gatsby-link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -7,6 +8,16 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Olá!</h1>
+    <ul>
+      <li>
+        <Link to="/" activeStyle={{ color: "cadetblue" }}>
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
   </Layout>
 );
 
