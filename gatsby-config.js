691ad4e0877dd709df9d237ad113637fb831fd62
Vinicius <vrf92@msn.com>
2020-07-29 21:40:32 -0300

    Alterando configuraçao inicial e removendo imagem do astronauta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Curso Gatsby`,
+    description: `A blog about frontend development.`,
+    author: `@devrodrigues`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -31,4 +31,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
