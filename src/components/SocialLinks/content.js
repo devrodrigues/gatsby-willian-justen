2d6db6673924efe9d272345b4fb0b3669236c644
Vinicius <vrf92@msn.com>
2020-08-03 21:14:19 -0300

    Instalando styled-icons e criando pasta SocialLinks

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/devrodrigues",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/",
+  },
+  {
+    label: "Youtube",
+    url: "https://youtube.com/",
+  },
+  {
+    label: "Instagram",
+    url: "https://instagram.com/",
+  },
+  {
+    label: "Unsplash",
+    url: "https://unsplash.com/",
+  },
+];
+
+export default links;
