8acfaf8b27eda5a2f4f51756c04d375dc0c39a47
Vinicius <vrf92@msn.com>
2020-08-06 13:17:32 -0300

    Criando template para home

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,19 @@
-const { createFilePath } = require(`gatsby-source-filesystem`);
 const path = require("path");
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
-// Para adicionar um campo slug para cada post
+// To add the slug field to each post
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
-  // Verifica se o arquivo é markdown com node.internal.type
+  // Ensures we are processing only markdown files
   if (node.internal.type === "MarkdownRemark") {
-    // Use createFilePath para criar o base path
+    // Use `createFilePath` to turn markdown files in our `data/faqs` directory into `/faqs/slug`
     const slug = createFilePath({
       node,
       getNode,
       basePath: "pages",
     });
 
-    // Cria o campo dentro de cada nó/post
+    // Creates new query'able field with name of 'slug'
     createNodeField({
       node,
       name: "slug",
@@ -22,51 +22,50 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 };
 
-//para criar qualquer página através da api node do Gatsby usamos o método abaixo
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
-
   return graphql(`
-  {
-    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
-      edges {
-        node {
-          fields {
-            slug
+    {
+      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
           }
-          frontmatter {
-            background
-            category
-            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
-            description
-            title
-          }
-          timeToRead
         }
       }
     }
   `).then((result) => {
     const posts = result.data.allMarkdownRemark.edges;
 
-    posts.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
-        /*para cada post ele criará uma página*/
-        component: path.resolve("./src/templates/blog-post.js"),
+        component: path.resolve(`./src/templates/blog-post.js`),
         context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
           slug: node.fields.slug,
         },
       });
     });
 
-    //limit
     const postsPerPage = 6;
     const numPages = Math.ceil(posts.length / postsPerPage);
 
     Array.from({ length: numPages }).forEach((_, index) => {
       createPage({
         path: index === 0 ? `/` : `/page/${index + 1}`,
-        component: path.resolve("./src/templates/blog-list.js"),
+        component: path.resolve(`./src/templates/blog-list.js`),
         context: {
           limit: postsPerPage,
           skip: index * postsPerPage,
