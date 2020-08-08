8fc5a508c7099fd1b65aeec87d3c4a7b90b819ff
Vinicius <vrf92@msn.com>
2020-08-07 21:12:22 -0300

    Integrando os posts ao componente Layout

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { graphql } from "gatsby";
 
+import Layout from "../components/Layout";
+import SEO from "../components/seo";
+
 //quando fazemos a query da forma abaixo, nosso componente recebe parâmetros via {data}
 const BlogPost = ({ data }) => {
   //para pegarmos todos os dados que precisamos do post
   const post = data.markdownRemark;
 
   return (
-    <>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
       <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-    </>
+    </Layout>
   );
 };
 
