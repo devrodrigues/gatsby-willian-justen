f9b8c1107930a601498d119cf39ae1419c75ef7f
Vinicius <vrf92@msn.com>
2020-08-11 11:59:21 -0300

    Envio do script de Abramov para troca de tema

--- src/html.js
+++ src/html.js
@@ -14,6 +14,37 @@ export default function HTML(props) {
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes} className="dark">
+        {/*script de Dan Abramov's*/}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function() {
+              window.__onThemeChange = function() {};
+
+              function setTheme(newTheme) {
+                window.__theme = newTheme;
+                preferredTheme = newTheme;
+                document.body.className = newTheme;
+                window.__onThemeChange(newTheme);
+              }
+              
+              var preferredTheme;
+              try {
+                preferredTheme = localStorage.getItem('theme');
+              } catch (err) { }
+
+              window.__setPreferredTheme = function(newTheme) {
+                setTheme(newTheme);
+                try {
+                  localStorage.setItem('theme', newTheme);
+                } catch (err) {}
+              }
+              
+              setTheme(preferredTheme || 'dark');
+            })();
+          `,
+          }}
+        />
         {props.preBodyComponents}
         <div
           key={`body`}
