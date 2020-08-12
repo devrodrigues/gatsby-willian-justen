057e8a8b6b1d13e52ca065e065d0d6276be77f46
Vinicius <vrf92@msn.com>
2020-08-12 13:19:27 -0300

    Definindo elementos fixos ao realizar transiçoes - Sidebar e Menubar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { TransitionPortal } from "gatsby-plugin-transition-link";
 import Sidebar from "../Sidebar";
 import Menubar from "../Menubar";
 
@@ -10,9 +11,13 @@ const Layout = ({ children }) => {
   return (
     <S.LayoutWrapper>
       <GlobalStyles />
-      <Sidebar />
+      <TransitionPortal level="top">
+        <Sidebar />
+      </TransitionPortal>
       <S.LayoutMain>{children}</S.LayoutMain>
-      <Menubar />
+      <TransitionPortal level="top">
+        <Menubar />
+      </TransitionPortal>
     </S.LayoutWrapper>
   );
 };
