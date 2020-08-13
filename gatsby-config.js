8a4bacdcce6411013437c08462ad4791093ecb92
Vinicius <vrf92@msn.com>
2020-08-13 11:29:55 -0300

    Alterando informaçoes do plugin gatsby-plugin-manifest para PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,13 +79,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Blog Front-end`,
+        short_name: `Blog devrodrigues`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site. - gera os favicons
       },
     },
     `gatsby-plugin-sitemap`,
